c1284f51452c530472de90be41daa12e780ab666
Ansh <anshkool1104@gmail.com>
2024-09-24 22:00:06 -0500

    Adding Template Code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/BasicAuto.java
@@ -0,0 +1,116 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Autonomous (name="BasicAuto")
+public class BasicAuto extends LinearOpMode {
+
+    private DcMotor FLeft;
+    private DcMotor FRight;
+    private DcMotor BRight;
+    private DcMotor BLeft;
+
+    @Override
+    public void runOpMode () {
+        BLeft = hardwareMap.get(DcMotor.class, "BLeft");
+        BRight = hardwareMap.get(DcMotor.class, "BRight");
+        FLeft = hardwareMap.get(DcMotor.class, "FLeft");
+        FRight = hardwareMap.get(DcMotor.class, "FRight");
+
+        // reverse the motors
+        BRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        FLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+
+        // this is what runs on the robot
+        while (opModeIsActive()) {
+
+            // goes straight at 1 speed for 1000 milliseconds(1 second)
+            runMotorsTime(1.0, 1000);
+
+        }
+    }
+
+    public void brakeMotors(){
+        BLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    public void runMotorsTime(double power, long motorTime) {
+        BLeft.setPower(power);
+        BRight.setPower(power * 0.95);
+        FRight.setPower(power * 0.95);
+        FLeft.setPower(power);
+
+        sleep(motorTime);
+
+        BLeft.setPower(0);
+        BRight.setPower(0);
+        FRight.setPower(0);
+        FLeft.setPower(0);
+    }
+
+    public void strafeMotorsRight(double power, long motorTime) {
+        BLeft.setPower(-power);
+        BRight.setPower(power);
+        FRight.setPower(-power);
+        FLeft.setPower(power);
+
+        sleep(motorTime);
+
+        BLeft.setPower(0);
+        BRight.setPower(0);
+        FRight.setPower(0);
+        FLeft.setPower(0);
+    }
+
+    public void strafeMotorsLeft(double power, long motorTime) {
+        BLeft.setPower(power);
+        BRight.setPower(-power);
+        FRight.setPower(power);
+        FLeft.setPower(-power);
+
+        sleep(motorTime);
+
+        BLeft.setPower(0);
+        BRight.setPower(0);
+        FRight.setPower(0);
+        FLeft.setPower(0);
+    }
+
+    public void turnRight(double power, long motorTime) {
+        BLeft.setPower(power);
+        BRight.setPower(-power);
+        FRight.setPower(-power);
+        FLeft .setPower(power);
+
+        sleep(motorTime);
+
+        BLeft.setPower(0);
+        BRight.setPower(0);
+        FRight.setPower(0);
+        FLeft.setPower(0);
+
+    }
+
+    public void turnLeft(double power, long motorTime) {
+        BLeft.setPower(-power);
+        BRight.setPower(power);
+        FRight.setPower(power);
+        FLeft.setPower(-power);
+
+        sleep(motorTime);
+
+        BLeft.setPower(0);
+        BRight.setPower(0);
+        FRight.setPower(0);
+        FLeft.setPower(0);
+    }
+}
