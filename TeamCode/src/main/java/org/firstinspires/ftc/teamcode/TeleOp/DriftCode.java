48a9f25322f8e300b7387c2ee89832a11c725ee4
anshthedev <anshkool1104@gmail.com>
2024-10-28 17:08:59 -0500

    Adding Operator Movements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DriftCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DriftCode.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp (name="DriftCode")
@@ -13,6 +14,8 @@ public class DriftCode extends OpMode {
     private DcMotor BRight;
     private DcMotor FLeft;
     private DcMotor FRight;
+    private DcMotor elevator;
+    private DcMotor arm;
 
     public void moveRobot(){
         double vertical;
@@ -30,18 +33,38 @@ public class DriftCode extends OpMode {
         BLeft.setPower((-pivot + (-vertical + horizontal)));
     }
 
-    public void init(){
+    public void moveArm() {
 
+        //TODO: Find these values with debugging
+        double maxPosition = 100;
+        double minPosition = 0;
+
+        double speed = gamepad2.left_stick_y;
+
+        if(arm.getCurrentPosition() <= maxPosition && speed > 0){
+            arm.setPower(gamepad2.left_stick_y);
+        }else if(arm.getCurrentPosition() >= minPosition && speed < 0){
+            arm.setPower(gamepad2.left_stick_y);
+        }else{
+            arm.setPower(0);
+        }
+    }
+
+    public void init(){
         // connect to hardware map
         BLeft = hardwareMap.get(DcMotor.class, "backleft");
         BRight = hardwareMap.get(DcMotor.class, "backright");
         FLeft = hardwareMap.get(DcMotor.class, "frontleft");
         FRight = hardwareMap.get(DcMotor.class, "frontright");
+        elevator = hardwareMap.get(DcMotor.class, "elevator");
+        arm = hardwareMap.get(DcMotor.class, "arm");
+
+        // setting encoders
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // reverse the motor directions
         BLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         FLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-
     }
 
     public void init_loop() {
@@ -50,5 +73,9 @@ public class DriftCode extends OpMode {
 
     public void loop(){
         moveRobot();
+        moveArm();
+
+        telemetry.addData("Arm Position", arm.getCurrentPosition());
+        telemetry.update();
     }
 }
