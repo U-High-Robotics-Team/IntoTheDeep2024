55c942f4ef57fcb77d86c74975fcb6fecd9067c6
anshthedev <anshkool1104@gmail.com>
2024-10-29 19:40:04 -0500

    Adding Encoder Limit and Fixing Logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/OperatorControls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/OperatorControls.java
@@ -12,36 +12,42 @@ public class OperatorControls extends OpMode {
     private DcMotor elevator;
     private DcMotor arm;
 
-    public void moveArm() {
-        //TODO: Find these values with debugging
-        double maxPosition = 100;
-        double minPosition = 0;
-
-        double speed = gamepad2.left_stick_y;
-
-        if(arm.getCurrentPosition() <= maxPosition && speed > 0){
-            arm.setPower(gamepad2.left_stick_y);
-        }else if(arm.getCurrentPosition() >= minPosition && speed < 0){
-            arm.setPower(gamepad2.left_stick_y);
-        }else{
-            arm.setPower(0);
-        }
-    }
+//    public void moveArm() {
+//        //TODO: Find these values with debugging
+//        double maxPosition = 100;
+//        double minPosition = 0;
+//
+//        double speed = gamepad2.right_stick_y;
+//
+//        if(arm.getCurrentPosition() <= maxPosition && speed > 0){
+//            arm.setPower(gamepad2.left_stick_y);
+//        }else if(arm.getCurrentPosition() >= minPosition && speed < 0){
+//            arm.setPower(gamepad2.left_stick_y);
+//        }else{
+//            arm.setPower(0);
+//        }
+//
+//        telemetry.addData("Arm Position", arm.getCurrentPosition());
+//        telemetry.update();
+//    }
 
     public void moveLift() {
         //TODO: Find these values with debugging
-        double maxPosition = 100;
-        double minPosition = 0;
+        double maxPosition = 0;
+        double minPosition = -6200;
 
-        double speed = gamepad2.right_stick_y;
+        double speed = gamepad2.left_stick_y;
 
-        if(elevator.getCurrentPosition() <= maxPosition && speed > 0){
-            elevator.setPower(gamepad2.right_stick_y);
-        }else if(elevator.getCurrentPosition() >= minPosition && speed < 0){
-            elevator.setPower(gamepad2.right_stick_y);
+        if(elevator.getCurrentPosition() <= maxPosition && speed >= 0){
+            elevator.setPower(gamepad2.left_stick_y);
+        }else if(elevator.getCurrentPosition() >= minPosition && speed <= 0){
+            elevator.setPower(gamepad2.left_stick_y);
         }else{
             elevator.setPower(0);
         }
+
+        telemetry.addData("Elevator Position", elevator.getCurrentPosition());
+        telemetry.update();
     }
 
     public void init(){
@@ -49,12 +55,11 @@ public class OperatorControls extends OpMode {
         elevator = hardwareMap.get(DcMotor.class, "elevator");
         arm = hardwareMap.get(DcMotor.class, "arm");
 
-        // setting encoders
-        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        elevator.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         elevator.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        elevator.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
 
         // if needed reverse the motor directions
 //        arm.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -66,11 +71,13 @@ public class OperatorControls extends OpMode {
 
 
     public void loop(){
-        moveArm();
+//        moveArm();
         moveLift();
 
+        telemetry.addData("Elevator Position", elevator.getCurrentPosition());
         telemetry.addData("Arm Position", arm.getCurrentPosition());
-        telemetry.addData("Arm Position", elevator.getCurrentPosition());
+        telemetry.addData("GamePad2.RightStickY", gamepad2.right_stick_y);
+        telemetry.addData("GamePad2.LeftStickY", gamepad2.left_stick_y);
         telemetry.update();
     }
 }
