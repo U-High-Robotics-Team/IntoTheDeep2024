39f57e071a2eaab3e3db7be2d8eeb4e1a1c4f943
anshthedev <anshkool1104@gmail.com>
2024-11-01 16:55:17 -0500

    Update DriftCode.java
    Still having these issues: Servo isn't running, moveArm() is messy and not accurate, moveLift() is not working correctly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DriftCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DriftCode.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp (name="DriftCode")
 public class DriftCode extends OpMode {
@@ -16,6 +17,10 @@ public class DriftCode extends OpMode {
     private DcMotor FRight;
     private DcMotor elevator;
     private DcMotor arm;
+    private Servo wrist;
+    private Servo holder;
+    private Servo wheel;
+
 
     public void moveRobot(){
         double vertical;
@@ -33,36 +38,64 @@ public class DriftCode extends OpMode {
         BLeft.setPower((-pivot + (-vertical + horizontal)));
     }
 
-    public void moveArm(){
-        double speed = -gamepad2.right_stick_y;
+    public void intake(){
+
+        if(gamepad2.a){
+            // take in
+            wheel.setPosition(1.0);
+        }else if(gamepad2.b){
+            // take out
+            wheel.setPosition(0.0);
+        }else{
+            wheel.setPosition(0.5);
+        }
 
 
-
-        arm.setPower(speed);
     }
 
+    public void moveArm() {
+        double speed = gamepad2.right_stick_y;
+
+        arm.setPower(speed);
+//        double currentPosition = arm.getCurrentPosition();
+//
+//
+//        double speedFactor = 10;
+//        double targetPosition = currentPosition + (speed * speedFactor);
+//
+//
+//        double maxPosition = 1000;
+//        double minPosition = 0;
+//
+//
+//        targetPosition = Math.max(minPosition, Math.min(maxPosition, targetPosition));
+//
+//
+//        double error = targetPosition - currentPosition;
+//
+//        double power = 0.01 * error;
+//
+//        arm.setPower(Math.max(-1, Math.min(1, power)));
+    }
+
+
     public void moveLift() {
-        // Define the limits
         double maxPosition = 6500; // Maximum position (top)
         double minPosition = 0; // Minimum position (bottom)
 
-        // Allows for sensitive input
-        double speed = Math.pow(gamepad2.left_stick_y, 3);
+        double speed = -Math.pow(gamepad2.left_stick_y, 3);
         telemetry.addData("Speed:", speed);
 
         // Determine if the elevator is within bounds
-        if (elevator.getCurrentPosition() < maxPosition && speed > 0) {
-            // Prevent moving up if at the max position
-            telemetry.addData("Elevator going out", 1);
+        if (elevator.getCurrentPosition() >= maxPosition && speed > 0) {
+            telemetry.addData("Elevator at max", 1);
+            elevator.setPower(0); // Stop power
+        }
+        else if (elevator.getCurrentPosition() <= minPosition && speed < 0) {
+            telemetry.addData("Elevator at min", 2);
+            elevator.setPower(0); // Stop power
+        }else{
             elevator.setPower(speed);
-        } else if (elevator.getCurrentPosition() > minPosition && speed < 0) {
-            // Prevent moving down if at the min position
-            telemetry.addData("Elevator going in", 2);
-            elevator.setPower(speed);
-        } else {
-            // Set elevator power based on joystick input if within bounds
-            telemetry.addData("Elevator stopped", 3);
-            elevator.setPower(0);
         }
     }
 
@@ -77,6 +110,9 @@ public class DriftCode extends OpMode {
         FRight = hardwareMap.get(DcMotor.class, "frontright");
         elevator = hardwareMap.get(DcMotor.class, "elevator");
         arm = hardwareMap.get(DcMotor.class, "arm");
+        wheel = hardwareMap.get(Servo.class, "wheel");
+        wrist = hardwareMap.get(Servo.class, "wrist");
+        holder = hardwareMap.get(Servo.class, "holder");
 
         // setting encoders
         elevator.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -85,11 +121,13 @@ public class DriftCode extends OpMode {
         arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        elevator.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
 
         // reverse the motor directions
         BLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         FLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        elevator.setDirection(DcMotorSimple.Direction.REVERSE);
         elevator.setDirection(DcMotorSimple.Direction.REVERSE);
 
 
@@ -103,8 +141,11 @@ public class DriftCode extends OpMode {
         moveRobot();
         moveLift();
         moveArm();
+        intake();
 
         telemetry.addData("Elevator Position", elevator.getCurrentPosition());
+        telemetry.addData("Arm Position", arm.getCurrentPosition());
+        telemetry.addData("Wheel Position", wheel.getPosition());
         telemetry.update();
     }
 }
