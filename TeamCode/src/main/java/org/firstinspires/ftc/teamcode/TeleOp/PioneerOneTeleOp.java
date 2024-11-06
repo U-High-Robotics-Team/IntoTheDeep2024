17c505a071f6703a177fdac032fb2c8c7b21c654
anshthedev <anshkool1104@gmail.com>
2024-11-06 11:13:56 -0600

    Adding Encoder Movements and Breaks
    Currently this version has everything working except for the current wrist movements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/PioneerOneTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/PioneerOneTeleOp.java
@@ -1,281 +1,194 @@
-package org.firstinspires.ftc.teamcode.TeleOp;
+package org.firstinspires.ftc.teamcode;
 
-
-
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-
 import com.qualcomm.robotcore.hardware.DcMotor;
-
 import com.qualcomm.robotcore.hardware.DcMotorController;
-
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-
 import com.qualcomm.robotcore.hardware.Servo;
 
-
-
-
-
-@TeleOp (name="PioneerOneTeleOp")
-
+// THIS OPMODE IS COPY AND PASTED AS THE ONBOT JAVA JUST SO WE CAN KEEP TRACK ON GITHUB
+@Disabled
+@TeleOp(name = "Pioneer One TeleOp")
 public class PioneerOneTeleOp extends OpMode {
 
     // Performance constants
-
-    final int SHOULDER_MAX = 1000;
-
+    final int SLIDE_Y_MAX = 6000;
+    final int SLIDE_X_MAX = 3000; // Maximum position (top)
+    final int SLIDE_MIN = 0; // Minimum position (bottom)
+    final double SLIDE_POWER = 1.0;
+    final int SHOULDER_MAX = 1500;
     final int SHOULDER_MIN = 0;
-
     final double SHOULDER_POWER = 0.5;
-
     final double WRIST_MAX = 0.7;
-
-    final double WRIST_MIN = 0;
-
+    final double WRIST_MIN = -1;
     final double CLAW_MAX = 0.6;
-
     final double CLAW_MIN = 0;
 
-
-
     // position targets
-
-    int shoulderTarget = 0;
-
+    double shoulderTarget = 0;
     double wristTarget = 0;
-
     double clawTarget = 0;
-
-
+    double slideTarget = 0;
 
     // initalizing motors
-
     private DcMotor BLeft;
-
     private DcMotor BRight;
-
     private DcMotor FLeft;
-
     private DcMotor FRight;
-
     private DcMotor slide;
-
     private DcMotor shoulder;
-
     private Servo wrist;
-
     private Servo holder;
-
     private Servo claw;
 
-
-
-
-
-    public void moveRobot(){
+    public void moveRobot() {
 
         double vertical;
-
         double horizontal;
-
         double pivot;
-
         double speed = 0.75;
 
-
-
         vertical = speed * gamepad1.left_stick_y;
-
         horizontal = speed * -gamepad1.left_stick_x;
-
         pivot = speed * -gamepad1.right_stick_x;
 
 
-
         FRight.setPower((pivot + (-vertical + horizontal)));
-
         BRight.setPower(pivot + (-vertical - horizontal));
-
         FLeft.setPower((-pivot + (-vertical - horizontal)));
-
         BLeft.setPower((-pivot + (-vertical + horizontal)));
-
     }
 
 
-
-    public void moveWrist(){
-
+    public void moveWrist() {
         double STEP = 0.02;
 
-        if(gamepad2.a){
+        if (gamepad2.a) {
+            // TODO: Try to make it binary operations
+            // wristTarget = Math.max(wristTarget - STEP, WRIST_MIN);
 
-            wristTarget = Math.max(wristTarget - STEP, WRIST_MIN);
+            wristTarget = WRIST_MIN;
 
-        }else if(gamepad2.y){
+        } else if (gamepad2.y) {
+            // TODO: Try to make it binary operations
+            // wristTarget = Math.min(wristTarget + STEP, WRIST_MAX);
 
-            wristTarget = Math.min(wristTarget + STEP, WRIST_MAX);
+            wristTarget = WRIST_MAX;
 
         }
 
-        wrist.setPosition(wristTarget);
-
-        telemetry.addData("Wrist Current / Target ", "(%.2f, %.2f)", -1.0,  wristTarget );
-
+        wrist.setPosition((int)wristTarget);
+        telemetry.addData("Wrist Current / Target ", "(%.2f, %.2f)", -1.0, wristTarget);
     }
 
 
-
-    public void moveClaw(){
-
+    public void moveClaw() {
         double STEP = 0.02;
 
-        if(gamepad2.x){
+        if (gamepad2.x) {
+            // TODO: Try to make it binary operations
+            // clawTarget = Math.max(clawTarget - STEP, CLAW_MIN);
 
-            clawTarget = Math.max(clawTarget - STEP, CLAW_MIN);
+            clawTarget = CLAW_MIN;
 
-        }else if(gamepad2.b){
+        } else if (gamepad2.b) {
+            // TODO: Try to make it binary operations
+            // clawTarget = Math.min(clawTarget + STEP, CLAW_MAX);
 
-            clawTarget = Math.min(clawTarget + STEP, CLAW_MAX);
+            clawTarget = CLAW_MAX;
 
         }
 
         claw.setPosition(clawTarget);
-
-        telemetry.addData("Claw Current / Target ", "(%.2f, %.2f)", -1.0,  clawTarget );
-
+        telemetry.addData("Claw Current / Target ", "(%.2f, %.2f)", -1.0, clawTarget);
     }
-
 
 
     public void moveShoulder() {
-
         final int STEP = 10;   // move increment
+        double input = STEP * -Math.pow(gamepad2.left_stick_y, 3);
 
-        // read control inputs and set target position, keeping within limits
+        shoulderTarget = Math.max(SHOULDER_MIN, Math.min(SHOULDER_MAX, shoulderTarget + input));
 
-        if (gamepad2.dpad_up){
+        // Previous DPad Controls
 
-            shoulderTarget = SHOULDER_MAX;
+        // if (gamepad2.dpad_up) {
+        //     shoulderTarget = SHOULDER_MAX;
 
-        } else if (gamepad2.dpad_down){
+        // } else if (gamepad2.dpad_down) {
+        //     shoulderTarget = SHOULDER_MIN;
 
-            shoulderTarget = SHOULDER_MIN;
+        // } else if (gamepad2.dpad_left) {
+        //     shoulderTarget = Math.max(shoulderTarget - STEP, SHOULDER_MIN);
 
-        } else if (gamepad2.dpad_left){
+        // } else if (gamepad2.dpad_right) {
+        //     shoulderTarget = Math.min(shoulderTarget + STEP, SHOULDER_MAX);
 
-            shoulderTarget = Math.max(shoulderTarget - STEP, SHOULDER_MIN);
+        // }
 
-        } else if (gamepad2.dpad_right){
-
-            shoulderTarget = Math.min(shoulderTarget + STEP, SHOULDER_MAX);
-
-        }
-
-        shoulder.setTargetPosition(shoulderTarget);
-
+        shoulder.setTargetPosition((int)shoulderTarget);
         shoulder.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
         shoulder.setPower(Math.abs(SHOULDER_POWER));
-
-        telemetry.addData("Shoulder Current / Target ", "(%d, %d)", shoulder.getCurrentPosition(), shoulderTarget );
-
+        telemetry.addData("Shoulder Current / Target ", "(%d, %.2f)", shoulder.getCurrentPosition(), shoulderTarget);
     }
-
 
 
     public void moveSlide() {
+        final int STEP = 30;
+        double input = STEP * -Math.pow(gamepad2.right_stick_y, 3);
+        double max;
 
-        double maxPosition = 6000; // Maximum position (top)
-
-        double minPosition = 0; // Minimum position (bottom)
-
-
-
-        double speed = Math.pow(gamepad2.left_stick_y, 3);
-
-        telemetry.addData("Speed:", speed);
-
-
-
-        // Determine if the elevator is within bounds
-
-        if (slide.getCurrentPosition() >= maxPosition && speed > 0) {
-
-            telemetry.addData("Slide at max", 1);
-
-            slide.setPower(0); // Stop power
-
-        } else if (slide.getCurrentPosition() <= minPosition && speed < 0) {
-
-            telemetry.addData("Slide at min", 2);
-
-            slide.setPower(0); // Stop power
-
+        if(shoulder.getCurrentPosition() > 500){
+            max = SLIDE_Y_MAX;
         }else{
-
-            slide.setPower(speed);
-
+            max = SLIDE_X_MAX;
         }
+        slideTarget = Math.max(SLIDE_MIN, Math.min(max, slideTarget + input));
 
+        //double power = -Math.pow(gamepad2.right_stick_y, 3);
+
+        // Determine if the elevator is outside of bounds and limit target
+        //if (power < 0) {
+        //    slideTarget = Math.max(slideTarget - STEP, SLIDE_MIN);
+        //} else if (power > 0) {
+        //    slideTarget = Math.min(slideTarget + STEP, SLIDE_MAX);
+        //}
+
+        slide.setTargetPosition((int)slideTarget);
+        slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        slide.setPower(Math.abs(SLIDE_POWER));
+        //slide.setPower(0);
+        telemetry.addData("Slide Current / Target ", "(%d, %.2f)", slide.getCurrentPosition(), slideTarget);
+        //telemetry.addData("GamePad2.RightStickY Power:", power);
     }
 
 
-
-    public void init(){
-
+    public void init() {
         // connect to hardware map
-
         BLeft = hardwareMap.get(DcMotor.class, "backleft");
-
         BRight = hardwareMap.get(DcMotor.class, "backright");
-
         FLeft = hardwareMap.get(DcMotor.class, "frontleft");
-
         FRight = hardwareMap.get(DcMotor.class, "frontright");
-
         slide = hardwareMap.get(DcMotor.class, "elevator");
-
         shoulder = hardwareMap.get(DcMotor.class, "arm");
-
-        claw = hardwareMap.get(Servo.class, "holder");
-
+        claw = hardwareMap.get(Servo.class, "claw");
         wrist = hardwareMap.get(Servo.class, "wrist");
 
-//        holder = hardwareMap.get(Servo.class, "holder");
-
-
-
         // setting encoders
-
         shoulder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
         slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-
-
-        // brakes
-
+        // set brakes
         shoulder.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
         slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-
-
         // reverse the motor directions
-
         BLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-
         FLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-
         slide.setDirection(DcMotorSimple.Direction.REVERSE);
-
-
-
     }
-
 
 
     public void init_loop() {
@@ -283,27 +196,15 @@ public class PioneerOneTeleOp extends OpMode {
     }
 
 
-
-
-
-    public void loop(){
-
+    public void loop() {
         moveRobot();
-
         moveShoulder();
-
         moveSlide();
-
         moveWrist();
-
         moveClaw();
 
-
-
         telemetry.addData("Slide Position", slide.getCurrentPosition());
-
         telemetry.update();
-
     }
 
 }
