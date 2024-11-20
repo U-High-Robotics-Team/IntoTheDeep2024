e47d846418dafcf9181ddbda8f0e340b64725e16
anshthedev <anshkool1104@gmail.com>
2024-11-19 23:01:54 -0600

    Replacing Methods and Converting C++ to Java
    Lots of TODO everywhere because those are the tasks we need to do to get this code up and working.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/CustomRoadRunner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/CustomRoadRunner.java
@@ -1,479 +1,346 @@
-#include <AccelStepper.h>
-
-        #include <MultiStepper.h>
-
-
-
-        #define STEPS_PER_REV 400
-
-        #define WHEEL_RAD 0.05
-
-        #define PI 3.14159
-
-        #define REVS_PER_METER 1 / (2 * PI * WHEEL_RAD)
-
-
-
-//front left, 1
-
-        #define STEPPER1_DIR_PIN 2
-
-        #define STEPPER1_STEP_PIN 7
-
-
-
-//back left, 2
-
-        #define STEPPER2_DIR_PIN 3
-
-        #define STEPPER2_STEP_PIN 8
-
-
-
-//middle, 3
-
-        #define STEPPER3_DIR_PIN 4
-
-        #define STEPPER3_STEP_PIN 9
-
-
-
-//back right, 4
-
-        #define STEPPER4_DIR_PIN 5
-
-        #define STEPPER4_STEP_PIN 10
-
-
-
-//front right, 5
-
-        #define STEPPER5_DIR_PIN 6
-
-        #define STEPPER5_STEP_PIN 11
-
-
-
-        #define MAX_STEP_SPEED 1500  // from library, 1000 steps/sec max
-
-
-
-        AccelStepper stepperFL(AccelStepper::DRIVER, STEPPER1_STEP_PIN, STEPPER1_DIR_PIN);
-
-        AccelStepper stepperBL(AccelStepper::DRIVER, STEPPER2_STEP_PIN, STEPPER2_DIR_PIN);
-
-        AccelStepper stepper3(AccelStepper::DRIVER, STEPPER3_STEP_PIN, STEPPER3_DIR_PIN);
-
-        AccelStepper stepperBR(AccelStepper::DRIVER, STEPPER4_STEP_PIN, STEPPER4_DIR_PIN);
-
-        AccelStepper stepperFR(AccelStepper::DRIVER, STEPPER5_STEP_PIN, STEPPER5_DIR_PIN);
-
-        MultiStepper steppers;
-
-
-
-        const float stepsPerRad = 62.832;
-
-        const float STEPS_PER_METER = STEPS_PER_REV / REVS_PER_METER;
-
-        const float invWheelRadius = 19.685;   // in meters
-
-        const float lengthCon = 0.3380;        //0.3975 half-width + half-length in meters
-
-        const float maxSpeed = 2;              //meters/sec
-
-        const float positionTolerance = 0.05;  //in meters
-
-        const float angleTolerance = 0.1;      //radians
-
-        const float maxRotSpeed = 0.1;        //radians / second
-
-        const float axleConst = 0.35;          //(width + height) / 2;
-
-        const float wheelRadius = 0.05;
-
-        const float TIMESTEP = 0.1;
-
-        char input[64] = "";
-
-        int inputIdx = 0;
-
-
-
-        void setup() {
-
-        Serial.begin(115200);
-
-        Serial.println("Serial started.");
-
-
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+@Disabled // using this so it won't cause errors when running other codes (hopefully works not tested)
+@Autonomous(name = "CustomRoadRunner")
+public class CustomRoadRunner extends LinearOpMode {
+
+    double STEPS_PER_REV = 400;
+    double WHEEL_RAD = 0.05;
+    double PI = Math.PI;
+    double REVS_PER_METER = 1 / (2 * PI * WHEEL_RAD);
+    double MAX_STEP_SPEED = 1500;
+
+    double stepsPerRad = 62.832;
+    double STEPS_PER_METER = STEPS_PER_REV / REVS_PER_METER;
+    double invWheelRadius = 19.685;   // in meters
+    double lengthCon = 0.3380;        //0.3975 half-width + half-length in meters
+    double maxSpeed = 2;              //meters/sec
+    double positionTolerance = 0.05;  //in meters
+    double angleTolerance = 0.1;      //radians
+    double maxRotSpeed = 0.1;        //radians / second
+    double axleConst = 0.35;          //(width + height) / 2;
+    double wheelRadius = 0.05;
+    double TIMESTEP = 0.1;
+
+    private DcMotor BLeft;
+    private DcMotor BRight;
+    private DcMotor FLeft;
+    private DcMotor FRight;
+    private DcMotor slide;
+    private DcMotor shoulder;
+    private Servo wrist;
+    private Servo claw;
+
+    // TODO: not sure what this does
+    char input[64]="";
+    int inputIdx = 0;
+
+    // TODO: need to figure out what setMinPulseWith is and corresponding methods for each
+    void setup() {
 
         int moveTo = 0;
-
         int minPulseWidth = 200;
 
-
-
         stepperFL.setMaxSpeed(MAX_STEP_SPEED);
-
         // stepperFL.setAcceleration(accel);
-
         stepperFL.setMinPulseWidth(minPulseWidth);
-
         steppers.addStepper(stepperFL);
 
-
-
         stepperFR.setMaxSpeed(MAX_STEP_SPEED);
-
         // stepperFR.setAcceleration(accel);
-
         stepperFR.setMinPulseWidth(minPulseWidth);
-
         steppers.addStepper(stepperFR);
 
-
-
         stepperBL.setMaxSpeed(MAX_STEP_SPEED);
-
         // stepperBL.setAcceleration(accel);
-
         stepperBL.setMinPulseWidth(minPulseWidth);
-
         steppers.addStepper(stepperBL);
 
 
-
         stepperBR.setMaxSpeed(MAX_STEP_SPEED);
-
         // stepperBR.setAcceleration(accel);
-
         stepperBR.setMinPulseWidth(minPulseWidth);
-
         steppers.addStepper(stepperBR);
 
-
-
-        Serial.println("Motors initialized.");
-
-
-
         //polarGo(3, 0, 0.5, -PI/2);
-
-        }
-
+    }
 
 
-        void loop() {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        BLeft = hardwareMap.get(DcMotor.class, "backleft");
+        BRight = hardwareMap.get(DcMotor.class, "backright");
+        FLeft = hardwareMap.get(DcMotor.class, "frontleft");
+        FRight = hardwareMap.get(DcMotor.class, "frontright");
+        slide = hardwareMap.get(DcMotor.class, "elevator");
+        shoulder = hardwareMap.get(DcMotor.class, "arm");
+        claw = hardwareMap.get(Servo.class, "claw");
+        wrist = hardwareMap.get(Servo.class, "wrist");
 
-        if (Serial.available()) {
+        // setting encoders
+        shoulder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        processSerialIn();
+        // set brakes
+        shoulder.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        }
+        // reverse the motor directions
+        BLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        FLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        slide.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        delay(1);
+        waitForStart();
+    }
 
-        }
+    void roboGo(double x, double y, double deg, double speed) {
 
+        double velo = 2;  // arbitrary - just for calculating steps
 
+        double dist = Math.sqrt((x * x) + (y * y));
 
-        void roboGo(float x, float y, float deg, float speed) {
+        double time = dist / velo;
 
-        const float velo = 2;  // arbitrary - just for calculating steps
+        double xVelo;
 
-        float dist = sqrt((x * x) + (y * y));
-
-        float time = dist / velo;
-
-        float xVelo;
-
-        float yVelo;
+        double yVelo;
 
         if (dist == 0) {
 
-        time = velo;
+            time = velo;
 
-        xVelo = 0;
+            xVelo = 0;
 
-        yVelo = 0;
+            yVelo = 0;
 
         } else {
 
-        xVelo = (x / dist) * velo;
+            xVelo = (x / dist) * velo;
 
-        yVelo = (y / dist) * velo;
+            yVelo = (y / dist) * velo;
 
         }
 
-        float rotVelo = (deg / time) * (M_PI / 180);
+        double rotVelo = (deg / time) * (PI / 180);
 
         // Serial.println(x / dist) ;
 
 
-
         //what type of var
 
-        float wFL = invWheelRadius * ((xVelo + yVelo) - (lengthCon * rotVelo));
+        double wFL = invWheelRadius * ((xVelo + yVelo) - (lengthCon * rotVelo));
+        double wFR = invWheelRadius * ((-xVelo + yVelo) + (lengthCon * rotVelo));
+        double wBL = invWheelRadius * ((-xVelo + yVelo) - (lengthCon * rotVelo));
+        double wBR = invWheelRadius * ((xVelo + yVelo) + (lengthCon * rotVelo));
 
-        float wFR = invWheelRadius * ((-xVelo + yVelo) + (lengthCon * rotVelo));
-
-        float wBL = invWheelRadius * ((-xVelo + yVelo) - (lengthCon * rotVelo));
-
-        float wBR = invWheelRadius * ((xVelo + yVelo) + (lengthCon * rotVelo));
-
-        // Serial.println(wFL);
-
-
-
-        float stepSpeedFL = wFL * stepsPerRad;
-
-        float stepSpeedFR = wFR * stepsPerRad;
-
-        float stepSpeedBL = wBL * stepsPerRad;
-
-        float stepSpeedBR = wBR * stepsPerRad;
-
-
+        double stepSpeedFL = wFL * stepsPerRad;
+        double stepSpeedFR = wFR * stepsPerRad;
+        double stepSpeedBL = wBL * stepsPerRad;
+        double stepSpeedBR = wBR * stepsPerRad;
 
         // instead of stuff below, set wheel speeds in multistepper and runSpeed
 
+        double stepsFL = (long) (stepSpeedFL * time);
+        double stepsFR = (long) (stepSpeedFR * time);
+        double stepsBL = (long) (stepSpeedBL * time);
+        double stepsBR = (long) (stepSpeedBR * time);
 
-
-        float stepsFL = (long)(stepSpeedFL * time);
-
-        float stepsFR = (long)(stepSpeedFR * time);
-
-        float stepsBL = (long)(stepSpeedBL * time);
-
-        float stepsBR = (long)(stepSpeedBR * time);
-
-
-
+        // TODO: move vs moveto?
         stepperFR.move(stepsFR);
-
         stepperFL.move(-stepsFL);
-
         stepperBR.move(stepsBR);
-
         stepperBL.move(-stepsBL);
 
 
+        // looks like this just resets positions?
+//        stepperFR.setCurrentPosition(0);
+//        stepperFL.setCurrentPosition(0);
+//        stepperBR.setCurrentPosition(0);
+//        stepperBL.setCurrentPosition(0);
 
-        stepperFR.setCurrentPosition(0);
-
-        stepperFL.setCurrentPosition(0);
-
-        stepperBR.setCurrentPosition(0);
-
-        stepperBL.setCurrentPosition(0);
+        BLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
 
 
-        long positions[4];
-
+        // TODO: need to understand this block of code
+        long positions[ 4];
         positions[0] = -stepsFL;
-
         positions[1] = stepsFR;
-
         positions[2] = -stepsBL;
-
         positions[3] = stepsBR;
-
         steppers.moveTo(positions);
 
-        }
+    }
 
 
-
-        void polarGo(float r, float theta, float speed, float rot) {
+    void polarGo(double r, double theta, double speed, double rot) {
 
         if (speed <= 0) {
 
-        speed = min(speed, maxSpeed);
+            speed = Math.min(speed, maxSpeed);
 
         }
 
+        //TODO: what is millis
         long timeStart = millis();
 
-        while (r > positionTolerance || abs(rot) > angleTolerance) {
-
-        Serial.print("rot");
-
-        Serial.println(rot);
+        while (r > positionTolerance || Math.abs(rot) > angleTolerance) {
 
 
+            if (millis() - timeStart > TIMESTEP * 1000) {
+                timeStart = millis();
 
-        if (millis() - timeStart > TIMESTEP * 1000) {
+                // recompute velocites for the next timestep
 
-        timeStart = millis();
+                double travelTime = r / speed;
+                double xVelo = speed * Math.cos(theta);
+                double yVelo = speed * Math.sin(theta);
 
-        // recompute velocites for the next timestep
+                //double rotVelo = max(-maxRotSpeed, (min(maxRotSpeed, rot / (r / speed))));
 
-        float travelTime = r / speed;
+                double rotVelo = speed;
+                wheelSpeeds(xVelo, yVelo, rotVelo);
 
-        float xVelo = speed * cos(theta);
+                // update targets for the next timestep
 
-        float yVelo = speed * sin(theta);
+                r -= speed * TIMESTEP;
+                rot -= rotVelo * TIMESTEP;
+                theta -= rotVelo * TIMESTEP;
+            }
 
-        //float rotVelo = max(-maxRotSpeed, (min(maxRotSpeed, rot / (r / speed))));
-
-        float rotVelo = speed;
-
-        Serial.print("rotvelo=");
-
-        Serial.println(rotVelo);
-
-        wheelSpeeds(xVelo, yVelo, rotVelo);
-
-        // update targets for the next timestep
-
-        r -= speed * TIMESTEP;
-
-        rot -= rotVelo * TIMESTEP;
-
-        theta -= rotVelo * TIMESTEP;
-
+            stepperFL.runSpeed();
+            stepperFR.runSpeed();
+            stepperBL.runSpeed();
+            stepperBR.runSpeed();
         }
+    }
 
-        stepperFL.runSpeed();
+    void wheelSpeeds(double xV, double yV, double rV) {
+//        stepperFL.setSpeed(-((xV - yV - axleConst * rV) * STEPS_PER_METER));  // +-- fl
+//        stepperFR.setSpeed((xV + yV + axleConst * rV) * STEPS_PER_METER);     // +++ fr
+//        stepperBL.setSpeed(-((xV + yV - axleConst * rV) * STEPS_PER_METER));  // ++-; bl
+//        stepperBR.setSpeed((xV - yV + axleConst * rV) * STEPS_PER_METER);     // +-+; br
+//
+        FLeft.setPower(-((xV - yV - axleConst * rV) * STEPS_PER_METER));
+        FRight.setPower((xV + yV + axleConst * rV) * STEPS_PER_METER);
+        BLeft.setPower(-((xV + yV - axleConst * rV) * STEPS_PER_METER));
+        BRight.setPower((xV - yV + axleConst * rV) * STEPS_PER_METER);
+    }
 
-        stepperFR.runSpeed();
-
-        stepperBL.runSpeed();
-
-        stepperBR.runSpeed();
-
-        }
-
-        Serial.println("Finished move.");
-
-        }
-
-
-
-        void wheelSpeeds(double xV, double yV, double rV) {
-
-        //
-
-        stepperFL.setSpeed(-((xV - yV - axleConst * rV) * STEPS_PER_METER));  // +-- fl
-
-        //Serial.print("wFl=");
-
-        //Serial.println(stepperFL.speed());
-
-        stepperFR.setSpeed((xV + yV + axleConst * rV)  * STEPS_PER_METER);     // +++ fr
-
-        stepperBL.setSpeed(-((xV + yV - axleConst * rV) * STEPS_PER_METER));  // ++-; bl
-
-        stepperBR.setSpeed((xV - yV + axleConst * rV) * STEPS_PER_METER);     // +-+; br
-
-        }
-
-
-
-        void processSerialIn() {
+    // TODO: understand what is serial
+    void processSerialIn() {
 
         Serial.println("Data received:");
 
-        float r = Serial.parseFloat();
-
-        float th = Serial.parseFloat();
-
-        float spd = Serial.parseFloat();
-
-        float rot = Serial.parseFloat();
+        double r = Serial.parsedouble();
+        double th = Serial.parsedouble();
+        double spd = Serial.parsedouble();
+        double rot = Serial.parsedouble();
 
         Serial.readStringUntil('\n');
 
         if (spd != 0 && (r != 0 || rot != 0)) {
+            Serial.println("Command sent.");
 
-        Serial.println("Command sent.");
-
-        polarGo(r, th, spd, rot);
-
+            polarGo(r, th, spd, rot);
         }
 
-        }
+    }
 
-
-
-        void moveX(double meters) {
-
+    void moveX(double meters) {
         double moveTo = meters * 1250;
+//
+//        stepperFL.moveTo(-moveTo);
+//        stepperFR.moveTo(moveTo);
+//        stepperBL.moveTo(-moveTo);
+//        stepperBR.moveTo(moveTo);
 
-        stepperFL.moveTo(-moveTo);
-
-        stepperFR.moveTo(moveTo);
-
-        stepperBL.moveTo(-moveTo);
-
-        stepperBR.moveTo(moveTo);
+        FLeft.setTargetPosition((int)-moveTo);
+        FRight.setTargetPosition((int)moveTo);
+        BLeft.setTargetPosition((int)-moveTo);
+        BRight.setTargetPosition((int)moveTo);
 
         go();
-
-        }
-
+    }
 
 
-        void spin(double degrees) {
-
+    void spin(double degrees) {
         double moveTo = degrees * 7.291666;  //2625 for 360
 
-        stepperFL.moveTo(moveTo);
+//        stepperFL.moveTo(moveTo);
+//        stepperFR.moveTo(moveTo);
+//        stepperBL.moveTo(moveTo);
+//        stepperBR.moveTo(moveTo);
 
-        stepperFR.moveTo(moveTo);
-
-        stepperBL.moveTo(moveTo);
-
-        stepperBR.moveTo(moveTo);
-
+        FLeft.setTargetPosition((int)moveTo);
+        FRight.setTargetPosition((int)moveTo);
+        BLeft.setTargetPosition((int)moveTo);
+        BRight.setTargetPosition((int)moveTo);
         go();
+    }
 
-        // + = cc
-
-        }
-
-
-
-        void moveY(double meters) {
-
+    void moveY(double meters) {
         double moveTo = meters * 1250;
 
-        stepperFL.moveTo(-moveTo);
+//        stepperFL.moveTo(-moveTo);
+//        stepperFR.moveTo(-moveTo);
+//        stepperBL.moveTo(moveTo);
+//        stepperBR.moveTo(moveTo);
 
-        stepperFR.moveTo(-moveTo);
-
-        stepperBL.moveTo(moveTo);
-
-        stepperBR.moveTo(moveTo);
+        FLeft.setTargetPosition((int)-moveTo);
+        FRight.setTargetPosition((int)-moveTo);
+        BLeft.setTargetPosition((int)moveTo);
+        BRight.setTargetPosition((int)moveTo);
 
         go();
-
-        }
-
+    }
 
 
-        void go() {
+    void go() {
+//        stepperFL.run();
+//        stepperBR.run();
+//        stepperBL.run();
+//        stepperFR.run();
 
-        stepperFL.run();
-
-        stepperBR.run();
-
-        stepperBL.run();
-
-        stepperFR.run();
-
-        }
+        FLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
 
 
+    void off() {
+//        stepperFL.setSpeed(0);
+//        stepperFR.setSpeed(0);
+//        stepperBL.setSpeed(0);
+//        steeperBR.setSpeed(0);
 
-        void off(){
+        FLeft.setPower(0);
+        BRight.setPower(0);
+        FRight.setPower(0);
+        BLeft.setPower(0);
+    }
+}
 
-        stepperFL.setSpeed(0);
-
-        stepperFR.setSpeed(0);
-
-        stepperBL.setSpeed(0);
-
-        }
