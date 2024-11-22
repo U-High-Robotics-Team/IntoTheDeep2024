8c3dd74ef7cdf6868313b267d206fb29a8d45fa3
anshthedev <anshkool1104@gmail.com>
2024-11-22 09:56:28 -0600

    Create TestingPIDController.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TestingPIDController.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.arcrobotics.ftclib.controller.PIDController;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@Autonomous
+public class TestingPIDController extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
