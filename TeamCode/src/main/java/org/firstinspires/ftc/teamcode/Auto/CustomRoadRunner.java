b236f97b24db26d366db206580ba71ad8b76258e
anshthedev <anshkool1104@gmail.com>
2024-11-23 14:12:26 -0600

    Removing Polar and Adjusted Constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/CustomRoadRunner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/CustomRoadRunner.java
@@ -3,12 +3,10 @@ package org.firstinspires.ftc.teamcode.Auto;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 //TODO: Notes for additions in projects
 // 1. Current coordinate system is relative to the robot and not the field so we
@@ -16,26 +14,37 @@ import com.qualcomm.robotcore.hardware.Servo;
 // 2. Going to add PID Controllers to handle errors (first isolating system within TestingPIDController.java
 // 3. Soon will be utilizing online player to see code run without robot in hand
 
-@Disabled // using this so it won't cause errors when running other codes (hopefully works not tested)
 @Autonomous(name = "CustomRoadRunner")
 public class CustomRoadRunner extends LinearOpMode {
 
     // motors
-    private DcMotor BLeft, BRight, FLeft, FRight;
+    DcMotorEx fRight;
+    DcMotorEx fLeft;
+    DcMotorEx bRight;
+    DcMotorEx bLeft;
+
+    double kP = 0.003; // bigger the error the faster we will fix it
+    double kI = 0.0001; // provides extra boost when you get close to the target
+    double kD = 0.00015; // dampens overshoot
+    double toleranceLevel = 5;
+    double bRError;
+    double bLError;
+    double fRError;
+    double fLError;
 
     // constants
-    private static final double WHEEL_RADIUS = 0.05;
+    private static final double WHEEL_RADIUS = 0.104;
     private static final double PI = Math.PI;
     private static final double REVS_PER_METER = 1 / (2 * PI * WHEEL_RADIUS);
     private static final double MAX_SPEED = 2; // meters per second
     private static final double TIMESTEP = 0.1; // seconds
     private static final double LENGTH_CONSTANT = 0.3380; // half-width + half-length in meters
     private static final double INV_WHEEL_RADIUS = 1 / WHEEL_RADIUS;
-    private static final double STEPS_PER_RAD = 62.832;
+    private static final double STEPS_PER_REV = 751.8;
+    private static final double STEPS_PER_RAD = STEPS_PER_REV/(2*PI);
     private static final double MAX_ROT_SPEED = 0.1; // radians per second
     private static final double POSITION_TOLERANCE = 0.05; // meters
     private static final double ANGLE_TOLERANCE = 0.1; // radians
-    private static final double STEPS_PER_REV = 1440;
     private static final double STEPS_PER_METER = STEPS_PER_REV * REVS_PER_METER;
     private static final double AXLE_CONSTANT = 0.35;          //(width + height) / 2;
 
@@ -47,32 +56,35 @@ public class CustomRoadRunner extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        BLeft = hardwareMap.get(DcMotor.class, "backleft");
-        BRight = hardwareMap.get(DcMotor.class, "backright");
-        FLeft = hardwareMap.get(DcMotor.class, "frontleft");
-        FRight = hardwareMap.get(DcMotor.class, "frontright");
+        bLeft = hardwareMap.get(DcMotorEx.class, "backleft");
+        bRight = hardwareMap.get(DcMotorEx.class, "backright");
+        fLeft = hardwareMap.get(DcMotorEx.class, "frontleft");
+        fRight = hardwareMap.get(DcMotorEx.class, "frontright");
 
         // setting encoders
-        BLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        BRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        FLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        FRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        // set brakes
-        BLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        BRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        FLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        FRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        bLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         // reverse the motor directions
-        BLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        FLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        bLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        fLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
         while (opModeIsActive()) {
             // move one meter at 45 degrees with speed 1 with no rotation
-            polarGo(1.0, Math.PI / 4, 1.0, 0);
+            telemetry.addData("Status: Begin Path", 1);
+            telemetry.update();
+            roboGo(1.0, 0, 0, 0.2);
+            telemetry.addData("Status: Completed Path", 0);
+            telemetry.update();
         }
     }
 
@@ -92,6 +104,13 @@ public class CustomRoadRunner extends LinearOpMode {
             yVelo = (y / dist) * velo;
 
         }
+
+        telemetry.addData("X Velocity", xVelo);
+        telemetry.addData("Y Velocity", yVelo);
+        telemetry.update();
+
+        sleep(5000);
+
 
         double rotVelo = (deg / time) * (PI / 180);
 
@@ -117,61 +136,102 @@ public class CustomRoadRunner extends LinearOpMode {
         double stepsBL = (long) (stepSpeedBL * time);
         double stepsBR = (long) (stepSpeedBR * time);
 
-        BLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        BRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        FLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        FRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        telemetry.addData("Front Left Target Steps", stepsFL);
+        telemetry.addData("Front Right Target Steps", stepsFR);
+        telemetry.addData("Back Left Target Steps", stepsBL);
+        telemetry.addData("Back Right Target Steps", stepsBR);
+        telemetry.update();
+        sleep(5000);
 
-        FLeft.setTargetPosition((int)stepsFL);
-        FRight.setTargetPosition((int)stepsFR);
-        BLeft.setTargetPosition((int)stepsBL);
-        BRight.setTargetPosition((int)stepsBR);
+        bLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        FLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        FRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        BLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        BRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        while(opModeIsActive()){
+            movePosition((int)stepsFR, (int)stepsFL, (int)stepsBR, (int)stepsBL);
+        }
 
-        FLeft.setPower(Math.abs(speed));
-        FRight.setPower(Math.abs(speed));
-        BLeft.setPower(Math.abs(speed));
-        BRight.setPower(Math.abs(speed));
     }
 
 
+    public void movePosition(int fRTargetSteps, int fLTargetSteps, int bRTargetSteps, int bLTargetSteps){
+        double bRLastError = 0;
+        double fRLastError = 0;
+        double fLLastError = 0;
+        double bLLastError = 0;
+        ElapsedTime timer = new ElapsedTime();
 
-    public void polarGo(double r, double theta, double speed, double rot) {
-        //TODO: replaced milis() with System.currentTimeMillis() but not sure if this is correct
-        long timeStart = System.currentTimeMillis();
+        this.fRError = fRTargetSteps - fRight.getCurrentPosition();
+        this.fLError = fLTargetSteps - fLeft.getCurrentPosition();
+        this.bRError = bRTargetSteps - bRight.getCurrentPosition();
+        this.bLError = bLTargetSteps - bLeft.getCurrentPosition();
 
-        // this loop helps find when we reach target position
-        while (r > POSITION_TOLERANCE || Math.abs(rot) > ANGLE_TOLERANCE) {
-            if (System.currentTimeMillis() - timeStart > TIMESTEP * 1000) {
-                timeStart = System.currentTimeMillis();
+        while(Math.abs(fRError) > toleranceLevel && opModeIsActive() && Math.abs(fLError) > toleranceLevel && Math.abs(bRError) > toleranceLevel && Math.abs(bLError) > toleranceLevel){
 
-                // recompute velocites for the next timestep
-                double travelTime = r / speed; //(t=d/v)
-                double xVelo = speed * Math.cos(theta);
-                double yVelo = speed * Math.sin(theta);
+            this.fRError = fRTargetSteps - fRight.getCurrentPosition();
+            this.fLError = fLTargetSteps - fLeft.getCurrentPosition();
+            this.bRError = bRTargetSteps - bRight.getCurrentPosition();
+            this.bLError = bLTargetSteps - bLeft.getCurrentPosition();
 
-                double rotVelo = speed; // worrying about rotation later
-                wheelSpeeds(xVelo, yVelo, rotVelo);
+            fRight.setPower(Math.max(Math.min(findPIDPower(fRTargetSteps, fRight, timer, fRLastError), 1), -1));
+            fLeft.setPower(Math.max(Math.min(findPIDPower(fLTargetSteps, fLeft, timer, fLLastError), 1), -1));
+            bRight.setPower(Math.max(Math.min(findPIDPower(bRTargetSteps, bRight, timer, bRLastError), 1), -1));
+            bLeft.setPower(Math.max(Math.min(findPIDPower(bLTargetSteps, bLeft, timer, bLLastError), 1), -1));
 
-                // update targets for the next timestep
-                r -= speed * TIMESTEP; // x = vt
-                rot -= rotVelo * TIMESTEP; // worrying about rotation later
-                theta -= rotVelo * TIMESTEP;// theta = wt
-            }
+            // setting for next iteration
+            bRLastError = bRError;
+            fRLastError = fRError;
+            fLLastError = fLError;
+            bLLastError = bLError;
+            timer.reset();
+
+            telemetry.addData("Front Right Position", fRight.getCurrentPosition());
+            telemetry.addData("Front Left Position", fLeft.getCurrentPosition());
+            telemetry.addData("Back Right Position", bRight.getCurrentPosition());
+            telemetry.addData("Back Left Position", bLeft.getCurrentPosition());
+
+            telemetry.addData("Front Right Power", Math.max(Math.min(findPIDPower(fRTargetSteps, fRight, timer, fRLastError), 1), -1));
+
+            telemetry.addData("Front Right Error", fRError);
+            telemetry.addData("Front Left Error", fLError);
+            telemetry.addData("Back Right Error", bRError);
+            telemetry.addData("Back Left Error", bLError);
+
+            telemetry.update();
         }
 
-        off();
+        // stopping motors
+        fRight.setPower(0);
+        fLeft.setPower(0);
+        bRight.setPower(0);
+        bLeft.setPower(0);
+    }
+
+    public double findPIDPower(int targetSteps, DcMotorEx motor, ElapsedTime timer, double lastError){
+        double derivative = 0;
+        double integralSum = 0;
+        double out = 0;
+        double error = 0;
+
+        error = targetSteps - motor.getCurrentPosition();
+
+        // rate of change of the error
+        derivative = (error - lastError) / timer.seconds();
+
+        // sum of all error over time
+        integralSum = integralSum + (error * timer.seconds());
+
+        out = (kP * error) + (kI * integralSum) + (kD * derivative);
+
+        return out;
     }
 
     private void stopMotors() {
-        FLeft.setPower(0);
-        FRight.setPower(0);
-        BLeft.setPower(0);
-        BRight.setPower(0);
+        fLeft.setPower(0);
+        fRight.setPower(0);
+        bLeft.setPower(0);
+        bRight.setPower(0);
     }
 
     private void wheelSpeeds(double xV, double yV, double rV) {
@@ -182,20 +242,20 @@ public class CustomRoadRunner extends LinearOpMode {
         double wBR = ((xV + yV) + (AXLE_CONSTANT * rV)) * STEPS_PER_METER;
 
         // set the velocities for each motor
-        FLeft.setPower(wFL);
-        FRight.setPower(wFR);
-        BLeft.setPower(wBL);
-        BRight.setPower(wBR);
+        fLeft.setPower(wFL);
+        fRight.setPower(wFR);
+        bLeft.setPower(wBL);
+        bRight.setPower(wBR);
     }
 
 
     public void moveX(double meters) {
         double moveTo = meters * STEPS_PER_METER;
 
-        FLeft.setTargetPosition((int)-moveTo);
-        FRight.setTargetPosition((int)moveTo);
-        BLeft.setTargetPosition((int)-moveTo);
-        BRight.setTargetPosition((int)moveTo);
+        fLeft.setTargetPosition((int)-moveTo);
+        fRight.setTargetPosition((int)moveTo);
+        bLeft.setTargetPosition((int)-moveTo);
+        bRight.setTargetPosition((int)moveTo);
 
         go();
     }
@@ -204,10 +264,10 @@ public class CustomRoadRunner extends LinearOpMode {
     public void spin(double degrees) {
         double moveTo = degrees * 7.291666;  //2625 for 360
 
-        FLeft.setTargetPosition((int)moveTo);
-        FRight.setTargetPosition((int)moveTo);
-        BLeft.setTargetPosition((int)moveTo);
-        BRight.setTargetPosition((int)moveTo);
+        fLeft.setTargetPosition((int)moveTo);
+        fRight.setTargetPosition((int)moveTo);
+        bLeft.setTargetPosition((int)moveTo);
+        bRight.setTargetPosition((int)moveTo);
 
         go();
     }
@@ -215,37 +275,37 @@ public class CustomRoadRunner extends LinearOpMode {
     public void moveY(double meters) {
         double moveTo = meters * STEPS_PER_METER;
 
-        FLeft.setTargetPosition((int)-moveTo);
-        FRight.setTargetPosition((int)-moveTo);
-        BLeft.setTargetPosition((int)moveTo);
-        BRight.setTargetPosition((int)moveTo);
+        fLeft.setTargetPosition((int)-moveTo);
+        fRight.setTargetPosition((int)-moveTo);
+        bLeft.setTargetPosition((int)moveTo);
+        bRight.setTargetPosition((int)moveTo);
 
         go();
     }
 
     public void go() {
-        FLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        FRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        BLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        BRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        fLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        fRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        bLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        bRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        FLeft.setPower(1);
-        FRight.setPower(1);
-        BLeft.setPower(1);
-        BRight.setPower(1);
+        fLeft.setPower(1);
+        fRight.setPower(1);
+        bLeft.setPower(1);
+        bRight.setPower(1);
 
         // wait for the motors to be finished
-        while (opModeIsActive() && (FLeft.isBusy() || FRight.isBusy() || BLeft.isBusy() || BRight.isBusy())) {
+        while(opModeIsActive() && (fLeft.isBusy() || fRight.isBusy() || bLeft.isBusy() || bRight.isBusy())) {
         }
 
         stopMotors();
     }
 
     public void off() {
-        FLeft.setPower(0);
-        BRight.setPower(0);
-        FRight.setPower(0);
-        BLeft.setPower(0);
+        fLeft.setPower(0);
+        bRight.setPower(0);
+        fRight.setPower(0);
+        bLeft.setPower(0);
     }
 }
 
